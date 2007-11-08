4d91bb97c6dd1d3e92374168d57aa33aaa8b2192
root <root>
2007-11-08 16:58:00 +0000

    *** empty log message ***

--- evdns.c
+++ evdns.c
@@ -1,4 +1,4 @@
-/* $Id: evdns.c,v 1.19 2007/11/06 18:49:32 root Exp $ */
+/* $Id: evdns.c,v 1.20 2007/11/08 16:58:00 root Exp $ */
 
 /* The original version of this module was written by Adam Langley; for
  * a history of modifications, check out the subversion logs.
